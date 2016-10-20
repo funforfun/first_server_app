959dad319d75ec27437b57163094bb98c6e46c48
s.kharchenko <s.kharchenko@mivar.ru>
2016-10-20 23:39:46 +0300

    убрал лишнее

--- JettyServer.java
+++ JettyServer.java
@@ -1,13 +1,6 @@
 package com.company;
 
-
-import org.eclipse.jetty.server.Handler;
-import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.ServerConnector;
-import org.eclipse.jetty.server.handler.AbstractHandler;
-import org.eclipse.jetty.server.handler.HandlerList;
-import org.eclipse.jetty.server.handler.ResourceHandler;
 
 /**
  * TODO:
@@ -20,40 +13,15 @@ public class JettyServer
     public static void main(String[] args) throws Exception
     {
         simpleServer();
-//        withStaticServer();
     }
 
-    private static void simpleServer() throws Exception, InterruptedException
+    private static void simpleServer() throws Exception
     {
         Frontend frontend = new Frontend();
-        Thread thread = new Thread(frontend);
-        thread.start();
+        (new Thread(frontend)).start();
 
         Server server = new Server(8080);
-//        server.setHandler(new JettyServer());
-//        server.start();
-//        server.join();
         server.setHandler(frontend);
-        server.start();
-        server.join();
-    }
-
-    private static void withStaticServer() throws Exception, InterruptedException {
-        Server server = new Server();
-        ServerConnector connector = new ServerConnector(server);
-        connector.setPort(8080);
-        server.addConnector(connector);
-
-        ResourceHandler resource_handler = new ResourceHandler();
-        resource_handler.setDirectoriesListed(true);
-        resource_handler.setWelcomeFiles(new String[]{ "index.html" });
-
-        resource_handler.setResourceBase(".");
-
-        HandlerList handlers = new HandlerList();
-        handlers.setHandlers(new Handler[] { resource_handler, (Handler) new JettyServer() });
-        server.setHandler(handlers);
-
         server.start();
         server.join();
     }
